55e58c48c402f01e599eaa07164ec49257e3c44c
BalkrishanSingh <Balkrishan62005@Gmail.com>
2021-12-08 12:55:04 +0530

    Solution for Day-7 Problem 1 and 2

---
+++ Day-7/Problem-2.py
@@ -0,0 +1,27 @@
+def get_input(file_name):
+    with open(file_name,'r') as f:
+        initial_positions = list(map(int,f.read().split(',')))
+    return initial_positions
+def calculate_fuel(goal,current_pos):
+    fuel_consumption = 0
+    if  goal > current_pos :
+        steps = (goal-current_pos)
+        fuel_consumption += (steps * (steps + 1)) // 2
+    elif goal < current_pos:
+        steps = (current_pos-goal)
+        fuel_consumption += (steps * (steps + 1)) // 2
+    elif goal == current_pos:
+        fuel_consumption += 0
+    return fuel_consumption
+def main():
+    initial_positions = get_input('Day-7/input.txt')
+    # initial_positions = [16,1,2,0,4,2,7,1,2,14] #Example input
+    fuel_consumption_each_round = [] 
+    for goal in initial_positions:
+        total_fuel_consumption = 0
+        for current_pos in initial_positions:
+            total_fuel_consumption += calculate_fuel(goal,current_pos)
+        fuel_consumption_each_round.append(total_fuel_consumption)
+    return min(fuel_consumption_each_round)
+if __name__ == '__main__' :
+    print(main())
